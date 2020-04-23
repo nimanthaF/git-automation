ac4c141c372d289e3e9e043307dab7ca3af02a0c
nimanthaF <mimanthafernando95@gmail.com>
2020-04-23 18:00:14 +0530

    automation for google search

--- automation.py
+++ automation.py
@@ -1,4 +1,11 @@
 from selenium import webdriver
 
-driver = webdriver.Firefox()
-driver.get("https://www.goal.com/en")
+driver=webdriver.Chrome()
+driver.get('https://www.google.lk/webhp?tab=rw')
+
+searchBox=driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input')
+searchBox.send_keys('tech with tharindu')
+
+searchButton=driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[3]/center/input[1]')
+searchButton.click()
+
